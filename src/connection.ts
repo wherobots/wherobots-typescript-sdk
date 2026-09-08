33a3fdf858d1912ed21b2438ded0225248d6e986
Clay McGinnis <github@clay.sh>
2026-09-08 16:20:29 -0700

    fix: preserve server messages in query errors (WBC-657)

--- src/connection.ts
+++ src/connection.ts
@@ -409,7 +409,7 @@ export class Connection {
             logger.child(errorEvent).error("Error event received");
             cleanup();
             abortSignal.removeEventListener("abort", handleSignalAborted);
-            reject(new Error("Error event received"));
+            reject(new Error(errorEvent.message));
             return;
           }
 
